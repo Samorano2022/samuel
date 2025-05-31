bbcf90eb856bbe9feb0a87f8e20a9331a3bd591b
Ayokanmi Samuel <ayokanmi.samuel@abbaandking.com>
2025-05-31 10:17:06 +0100

    special case study update

--- src/app/weddingcase/weddingcase.component.ts
+++ src/app/weddingcase/weddingcase.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-weddingcase',
   standalone: true,
   imports: [],
   templateUrl: './weddingcase.component.html',
-  styleUrl: './weddingcase.component.css'
+  styleUrl: './weddingcase.component.css',
 })
 export class WeddingcaseComponent {
-
+  constructor(private router: Router) {}
+  onHome() {
+    this.router.navigateByUrl('home');
+  }
+  onProject() {
+    this.router.navigateByUrl('project');
+  }
 }
